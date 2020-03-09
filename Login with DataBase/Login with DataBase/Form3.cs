2612f2c43fc6963ae25c8335f9c4f8cf2458e20a
Boburshoh <152120171073@ogrenci.ogu.edu.tr>
2020-03-09 12:33:42 +0200

    TSV file saving

--- Login with DataBase/Login with DataBase/Form3.cs
+++ Login with DataBase/Login with DataBase/Form3.cs
@@ -52,7 +52,7 @@ namespace Login_with_DataBase
 
             StreamWriter sw = File.AppendText(@path1);
             string[] lines = File.ReadAllLines(Form1.path + "/mydb.csv");
-			if (save.ShowDialog()==DialogResult.Cancel)
+			if (save.ShowDialog()==DialogResult.OK)
 			{
 				for (int i = 0; i < lines.Length; i++)
 				{
